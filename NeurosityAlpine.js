55e4bc7f1f4914153fdc9ee7adc06844d3290897
Richard Fox <richard.fox@ahref.co.uk>
2026-01-04 16:00:28 -0800

    start claiming device workflow

--- NeurosityAlpine.js
+++ NeurosityAlpine.js
@@ -1,5 +1,6 @@
 import { createObservableHelper } from './ObservableHelper'
 
+const LOG_NETWORKS = false;
 // Abstracts neurosity stuff from alpine
 /**
  * @param {Neurosity} client
@@ -8,10 +9,13 @@ export function createNeurosityAlpine(client) {
     var helper;
     return {
         client,
+
         email: '',
         password: '',
+
         ssid: '',
         wifiPassword: '',
+
         authenticated: false,
         user: null,
         state: '',
@@ -19,19 +23,35 @@ export function createNeurosityAlpine(client) {
 
         init() {
             helper = createObservableHelper(this);
+
+            // AUTH
             helper.subscribe(this.client.onAuthStateChanged(), 'user');
             helper.subscribe(this.client.onAuthStateChanged(), 'authenticated', user => user != null);
 
+            // Connection state
             // *   // { streamingMode: "wifi-only", activeMode: "wifi", connected: true }
-
             helper.subscribe(this.client.streamingState(), 'state', state => `Mode: ${state.streamingMode}. activeMode: ${state.activeMode}, connected: ${state.connected}`);
             helper.subscribe(this.client.streamingState(), 'bluetooth', state => state.activeMode == 'bluetooth');
 
-            // this.client.bluetooth.wifi.nearbyNetworks().subscribe(a => console.log(a));
+            // Disable by default due to security/console spam
+            if (LOG_NETWORKS)
+            {
+                this.client.bluetooth.wifi.nearbyNetworks().subscribe(a => console.log(a)); 
+            }
         },
 
         login() {
             return this.client.login({ email: this.email, password: this.password });
+        },
+
+        async hasDevice(deviceId) {
+            var devices = await this.client.getDevices();
+            var found = devices.some(device => device.deviceId);
+            return found;
+        },
+
+        async claimDevice(deviceId) {
+            this.client.claimDevice(deviceId);
         },
 
         connect() {
